5e16087af9ea02398b3faa59b2356abad2024342
Romain Quidet <contact@xdappfactory.com>
2019-03-23 20:45:57 +0100

    Initial Commit

---
+++ GPXToolKit/GPXToolKit.h
@@ -0,0 +1,19 @@
+//
+//  GPXToolKit.h
+//  GPXToolKit
+//
+//  Created by Romain Quidet on 23/03/2019.
+//  Copyright © 2019 Romain Quidet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GPXToolKit.
+FOUNDATION_EXPORT double GPXToolKitVersionNumber;
+
+//! Project version string for GPXToolKit.
+FOUNDATION_EXPORT const unsigned char GPXToolKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GPXToolKit/PublicHeader.h>
+
+
